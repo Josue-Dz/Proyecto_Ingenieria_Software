45457e92cfe57ea3922b9e9c664811a970fa82a7
deibi <dparad456@gmail.com>
2026-02-16 12:51:51 -0600

    creacion de backend java spring

---
+++ backend/projecto-ingenieria/src/test/java/edu/unah/hn/projecto_ingenieria/ProjectoIngenieriaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unah.hn.projecto_ingenieria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoIngenieriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
