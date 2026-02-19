1f8deabec27d2f3a3ff39fbeb23a5531055221cb
jdnunez-10 <jdnunezo@unah.hn>
2026-02-18 21:38:33 -0600

    Se agregan los DTOS

---
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/DTO/TableroDTO.java
@@ -0,0 +1,13 @@
+package edu.unah.hn.projecto_ingenieria.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TableroDTO {
+    
+    private Long idTablero;
+
+    private Long idProyecto;
+}
