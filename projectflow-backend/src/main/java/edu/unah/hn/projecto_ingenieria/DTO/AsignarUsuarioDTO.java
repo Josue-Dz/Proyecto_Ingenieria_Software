f06947a35888d0449ccc4a301efd93c315c0d142
jdnunez-10 <jdnunezo@unah.hn>
2026-02-18 21:54:09 -0600

    Se agregan los DTOS

---
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/DTO/AsignarUsuarioDTO.java
@@ -0,0 +1,14 @@
+package edu.unah.hn.projecto_ingenieria.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AsignarUsuarioDTO {
+
+    private Long idUsuario;
+    
+    private Long idTarjeta;
+}
+
---
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/DTO/ProyectoResponseDTO.java
@@ -0,0 +1,26 @@
+package edu.unah.hn.projecto_ingenieria.DTO;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ProyectoResponseDTO {
+    
+    private Long idProyecto;
+
+    private String nombreProyecto;
+
+    private String descripcion;
+
+    private LocalDate fechaInicio;
+    
+    private LocalDate fechaFin;
+}
