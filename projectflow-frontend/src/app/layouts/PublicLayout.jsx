f6c19ffba244451485cea643f732c1bc6b3ad1d7
Josue-Dz <ronnyjosueposadas4@gmail.com>
2026-02-24 11:37:49 -0600

    feat: Crear Landing Page, páginas de login y registro de usuarios

---
+++ projectflow-frontend/src/app/layouts/PublicLayout.jsx
@@ -0,0 +1,36 @@
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+
+const PublicLayout = () => {
+    return (
+        <>
+            <div className="min-h-screen w-full relative">
+                <div
+                    className="absolute inset-0 z-0"
+                    style={{
+                        background: "radial-gradient(125% 125% at 50% 10%, #000000 40%, #2b0707 100%)",
+                    }}
+                />
+
+                <div className='relative z-10 flex flex-col min-h-screen overflow-x-hidden'>
+                    
+                        <header>
+                            <Navbar />
+                        </header>
+
+                        <main className='px-4 sm:px-6 md:px-12 max-w-7xl mx-auto pt-22'>
+                            <Outlet /> {/* Importante aquí se renderizarán las páginas hijas */}
+                        </main>
+
+                        <footer>
+                            <Footer />
+                        </footer>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PublicLayout
---
+++ projectflow-frontend/src/features/landing/components/FeaturesSection.jsx
@@ -0,0 +1,49 @@
+<i class="bi bi-people-fill"></i>
+
+const FeaturesSection = () => {
+    return (
+        <section>
+            <div className='flex flex-col py-6 px-20 justify-between gap-5 md:flex-row text-white'>
+
+                <div className='border border-white/25 rounded w-full md:w-1/3 p-3 min-w-32 max-w-sm mx-auto hover:scale-104 transition-transform duration-300'>
+
+                    <span className="material-symbols-rounded">view_kanban</span>
+
+                    <h2 className='pb-1'>Tableros Kanban</h2>
+
+                    <p className='text-white/75'>
+                        Organiza tus tareas de manera visual y dinámica. Arrastra y suelta actividades, controla el progreso y mantén la claridad en cada proyecto.
+                    </p>
+
+                </div>
+
+                <div className='border border-white/25 rounded w-full md:w-1/3 p-3 min-w-32 max-w-sm mx-auto hover:scale-104 transition-transform duration-300'>
+
+                    <span className="material-symbols-rounded">groups</span>
+
+                    <h2 className='pb-1'>Gestión de Equipos</h2>
+
+                    <p className='text-white/75'>
+                        Coordina fácilmente a tu equipo. Asigna responsabilidades, comparte avances y fomenta la colaboración en un espacio centralizado.
+                    </p>
+
+                </div>
+
+                <div className='border border-white/25 rounded w-full md:w-1/3 p-3 min-w-32 max-w-sm mx-auto hover:scale-104 transition-transform duration-300'>
+
+                    <span className="material-symbols-rounded">folder_open</span>
+
+                    <h2 className='pb-1'>Proyectos Académicos</h2>
+
+                    <p className='text-white/75'>
+                        Administra tus trabajos y proyectos educativos. Lleva un registro ordenado de entregas, plazos y recursos para alcanzar tus metas académicas.
+                    </p>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default FeaturesSection
