78a84d2506c1cb05cc81dc26fc5005c9e203b947
jdnunez-10 <jdnunezo@unah.hn>
2026-03-07 23:08:00 -0600

    feat: Se agrega la entidad de TarjetaXColumna junto a su repositorio

---
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/Entity/TarjetaXColumna.java
@@ -0,0 +1,42 @@
+package edu.unah.hn.projecto_ingenieria.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tarjeta_x_columna")
+@IdClass(TarjetaXColumnaId.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TarjetaXColumna {
+    
+    @Id
+    @Column(name = "id_tarjeta")
+    private Long idTarjeta;
+
+    @Id
+    @Column(name = "id_columna")
+    private Long idColumna;
+
+    private Integer posicion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_columna", insertable = false, updatable = false)
+    private Columna columna;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tarjeta", insertable = false, updatable = false)
+    private Tarjeta tarjeta;
+}
---
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/Entity/TarjetaXColumnaId.java
@@ -0,0 +1,24 @@
+package edu.unah.hn.projecto_ingenieria.Entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class TarjetaXColumnaId implements Serializable {
+
+    private Long idTarjeta;
+
+    private Long idColumna;
+
+    // Getters, setters, equals, and hashCode methods
+    
+}
