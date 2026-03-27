c280cdcf1596842acbaa96f37d8d9b530c43f45b
jdnunez-10 <jdnunezo@unah.hn>
2026-03-27 06:19:43 -0600

    "update: Se modifica en el frontend para que permita las notificaciones en tiempor real"

--- projectflow-frontend/src/features/boards/services/notificationService.js
+++ projectflow-frontend/src/features/boards/services/notificationService.js
@@ -1,4 +1,7 @@
 import apiClient from "../../../shared/api/apiCliente";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
+
 
 export const getMisNotificacionesRequest = async () => {
     const response = await apiClient.get("/notifications");
@@ -17,4 +20,38 @@ export const marcarComoLeidaRequest = async (id) => {
 
 export const marcarTodasComoLeidasRequest = async () => {
     await apiClient.put("/notifications/read/all");
+};
+
+// WebSocket 
+
+let stompClient = null;
+
+export const conectarNotificaciones = (onNotificacion) => {
+    const token = localStorage.getItem("token"); // mismo lugar que tu interceptor de axios
+
+    stompClient = new Client({
+        webSocketFactory: () =>
+            new SockJS(`http://localhost:8080/ws?token=${token}`),
+
+        reconnectDelay: 5000,
+
+        onConnect: () => {
+            console.log("WebSocket conectado");
+            stompClient.subscribe("/user/queue/notificaciones", (message) => {
+                const notificacion = JSON.parse(message.body);
+                onNotificacion(notificacion);
+            });
+        },
+
+        onDisconnect: () => console.log("WebSocket desconectado"),
+
+        onStompError: (frame) =>
+            console.error("Error STOMP:", frame.headers["message"]),
+    });
+
+    stompClient.activate();
+};
+
+export const desconectarNotificaciones = () => {
+    stompClient?.deactivate();
 };
