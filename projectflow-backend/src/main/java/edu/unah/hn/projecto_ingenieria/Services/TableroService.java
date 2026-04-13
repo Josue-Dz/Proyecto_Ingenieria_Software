15ea4a2cfd1e64eb9e2ffa44dd64426570e04478
Josue-Dz <ronnyjosueposadas4@gmail.com>
2026-04-12 23:47:14 -0600

    fix: Eficiencia Dashboard

--- projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/Services/TableroService.java
+++ projectflow-backend/src/main/java/edu/unah/hn/projecto_ingenieria/Services/TableroService.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
